52b78e40e43a3d7093acce81e97226714ee1e0fd
Alexisvib <alexis.vib@gmail.com>
2021-04-13 19:18:02 +0200

    Feature : possibility to print video by style the button select do not work for the moment, i add the logo and change the icon in the layout

--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -2,17 +2,35 @@
 
 namespace App\Controller;
 
+use App\Model\SongStyleManager;
+use App\Model\StyleManager;
 use App\Model\VideoManager;
 
 class VideoController extends AbstractController
 {
-    public function video(): string
+    public function all(): string
     {
         $videoManager = new VideoManager();
+        $styleManager = new StyleManager();
         $videos = $videoManager->selectAll();
+        $styles = $styleManager->selectAll();
 
-        return $this->twig->render('Item/video.html.twig', ['videos' => $videos]);
+        return $this->twig->render('Item/video.html.twig', [
+            'videos' => $videos,
+            'styles' => $styles,
+        ]);
     }
 
-
+    public function style($style): string
+    {
+        $songStyleManager = new SongStyleManager();
+        $styleManager = new StyleManager();
+        $styles = $styleManager->selectAll();
+        $videos = $songStyleManager->selectByStyle($style);
+        return $this->twig->render('Item/videoByStyle.html.twig', [
+            'videos' => $videos,
+            'styles' => $styles,
+            'styleMusic' => $style
+        ]);
+    }
 }
