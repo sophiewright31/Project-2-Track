c5e9b4d9b836d48e3456ce34616261112f113c59
Alexisvib <alexis.vib@gmail.com>
2021-04-21 16:00:58 +0200

    Feature : possibility to select style when adding a song

--- src/Controller/DJSetController.php
+++ src/Controller/DJSetController.php
@@ -2,10 +2,16 @@
 
 namespace App\Controller;
 
+use App\Model\StyleManager;
+
 class DJSetController extends AbstractController
 {
-    public function index()
+    public function index(): string
     {
-        return $this->twig->render('djset/djhome.html.twig');
+        $styleManager = new StyleManager();
+        $styles = $styleManager->selectAll();
+        return $this->twig->render('djset/djhome.html.twig', [
+            'styles' => $styles
+        ]);
     }
 }
